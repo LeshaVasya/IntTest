3e4478477da27b3453151f2f55e6f515e22646fa
Alexey Vasiliev <alexey.vasiliev@kohls.com>
2018-04-29 09:58:59 +0300

    Add virtual methods to StaticDemo

--- src/com/company/StaticDemo.java
+++ src/com/company/StaticDemo.java
@@ -7,17 +7,28 @@ package com.company;
 class Vehicle{
   public static void  kmToMiles(int km){
     System.out.println("Внутри родительского класса/статического метода");
-  } }
+  }
+  public  void  minToSec(int min){
+    System.out.println("Внутри родительского класса виртуального метода");
+  }
+}
 
 class Car extends Vehicle{
   public static void  kmToMiles(int km){
     System.out.println("Внутри дочернего класса/статического метода ");
-  } }
+  }
+  public  void  minToSec(int min){
+    System.out.println("Внутри дочернего класса виртуального метода");
+  }
+
+}
 
 public class StaticDemo{
   public static void main(String args[]){
     Vehicle v = new Car();
     v.kmToMiles(10);
+    v.minToSec(60);
     Vehicle.kmToMiles(10);
     Car.kmToMiles(10);
-  }}
+  }
+}
